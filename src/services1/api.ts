ab0f960d9ef61df1f6845787b69159698139eee1
AnandChaudhary590 <anandchaudhary76900@gmail.com>
2026-09-06 07:38:39 +0530

    Fix production backend API URL

--- src/services1/api.ts
+++ src/services1/api.ts
@@ -1,7 +1,16 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: "https://cloud-storage-backend-u4cd.onrender.com/api",
 });
 
 export default api;
